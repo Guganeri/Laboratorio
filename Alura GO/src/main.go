246fe51994a734e564b985902016d96bdace3c7b
Guga Neri <guganeriss@gmail.com>
2020-07-18 18:42:32 -0300

    Aula 02

--- Alura GO/src/main.go
+++ Alura GO/src/main.go
@@ -9,20 +9,34 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucessso"
+	} else {
+		return "Saldo insuficiente"
+	}
+
+}
+
 func main() {
 	//var contaDoGustavo ContaCorrente = ContaCorrente{}
-	contaDoGustavo := ContaCorrente{titular: "Gustavo", numeroAgencia: 001, numeroConta: 123456, saldo: 125.50}
+	//contaDoGustavo := ContaCorrente{titular: "Gustavo", numeroAgencia: 001, numeroConta: 123456, saldo: 125.50}
+	//fmt.Println(contaDoGustavo)
 
-	contaBruna := ContaCorrente{"Bruna", 222, 11122, 120}
+	//var contaDaCris *ContaCorrente //* -> alocar espaço na memoria para variável
+	//contaDaCris = new(ContaCorrente)
+	//contaDaCris.titular = "Cris"
+	//contaDaCris.saldo = 500
 
-	fmt.Println(contaDoGustavo)
-	fmt.Println(contaBruna)
+	contaDaSilva := ContaCorrente{}
+	contaDaSilva.titular = "Silvia"
+	contaDaSilva.saldo = 500
 
-	var contaDaCris *ContaCorrente //* -> alocar espaço na memoria para variável
-	contaDaCris = new(ContaCorrente)
-	contaDaCris.titular = "Cris"
-	contaDaCris.saldo = 500
+	fmt.Println(contaDaSilva.saldo)
 
-	fmt.Println(contaDaCris)
+	fmt.Println(contaDaSilva.Sacar(-300))
+	fmt.Println(contaDaSilva.saldo)
 
 }
