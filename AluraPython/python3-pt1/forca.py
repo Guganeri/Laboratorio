8078531eab1f7755e15a58f95da5447912264055
Guga Neri <guganeriss@gmail.com>
2020-09-04 14:46:01 -0700

    curso 02

--- AluraPython/python3-pt1/forca.py
+++ AluraPython/python3-pt1/forca.py
@@ -4,6 +4,28 @@ def jogar():
     print("Bem-vindo ao jogo forca")
     print("*****************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    #enquanto não enforcou e não acertou
+    while(not enforcou and not acertou):
+        
+        chute = input("Qual letra? ")
+        chute = chute.strip()        
+
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index+1))
+            index = index+1
+
+
+    print("Fim de jogo")
+
+
 
 if (__name__ == "__main__"):
     jogar()
