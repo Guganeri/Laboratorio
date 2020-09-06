4722868f23e519691e7d728e3b43acc4487e0e4f
Guga Neri <guganeriss@gmail.com>
2020-09-06 11:47:14 -0700

    python pt2

--- AluraPython/python3-pt1/forca.py
+++ AluraPython/python3-pt1/forca.py
@@ -1,10 +1,29 @@
+import random
+import os
 
 def jogar():
     print("*****************************")
     print("Bem-vindo ao jogo forca")
     print("*****************************")
 
-    palavra_secreta = "banana"
+
+
+    print(os.system("pwd"))
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo: 
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    print(palavra_secreta)
+    
+    letras_acertadas = ["_" for letra in palavra_secreta]
+    erros = 0 
 
     enforcou = False
     acertou = False
@@ -13,18 +32,28 @@ def jogar():
     while(not enforcou and not acertou):
         
         chute = input("Qual letra? ")
-        chute = chute.strip()        
+        chute = chute.strip().upper()
 
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Ops, Você errou ! faltam {} tentativas.".format(6-erros))
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, index+1))
-            index = index+1
+        enforcou = erros  == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+    
+    if (acertou):
+        print("Você ganhou !!!")
+    else:
+        print("Você perdeu !!! D:")
 
-
-    print("Fim de jogo")
-
+    print("Fim do jogo")
 
 
 if (__name__ == "__main__"):
