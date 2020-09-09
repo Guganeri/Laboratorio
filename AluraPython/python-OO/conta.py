09384556fd78435f0d1404833492902f55ad728a
Guga Neri <guganeriss@gmail.com>
2020-09-08 19:42:48 -0700

    OO Py

--- AluraPython/python-OO/conta.py
+++ AluraPython/python-OO/conta.py
@@ -3,8 +3,31 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto ...")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
     
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
+
+    def depositar(self, valor):
+        self.__saldo += valor
+    
+    def sacar(self, valor):
+        self.__saldo -= valor
+
+    def transfe(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
+
+
+
+    def pega_saldo(self):
+        return self.__saldo
+
+    def pega_titular(self):
+        return self.__titular
+
+    def pega_limite(self):
+        return self.__limite
