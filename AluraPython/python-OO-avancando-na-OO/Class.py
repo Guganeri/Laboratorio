ac69d68a456abb2a90aa76e475aa7fb01c79a004
Guga Neri <guganeriss@gmail.com>
2020-09-14 17:22:08 -0700

    Modulo 03 - Python OO Avancado

--- AluraPython/python-OO-avancando-na-OO/Class.py
+++ AluraPython/python-OO-avancando-na-OO/Class.py
@@ -18,12 +18,18 @@ class Programa:
     @nome.setter
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
+    
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.likes} Like(s)'
 
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.duracao} - {self.likes} Like(s)'
         
 
 class Serie(Programa):
@@ -31,15 +37,24 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.temporadas} Temporada(s) - {self.likes} Like(s)'
+
 
 vingadores = Filme("vingadores - guerra Infinita", "2018", "03 horas")
 vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
-      f'- Duracao: {vingadores.duracao} - Likes: {vingadores.likes}')
+#print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
+#       f'- Duracao: {vingadores.duracao} - Likes: {vingadores.likes}')
 
 atlanta = Serie("atlanta", "2018", "02")
 atlanta.dar_like()
 atlanta.dar_like()
 
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
-      f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+#print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
+#       f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    print(programa)
