956b27f5a3d8ef9cfbfc5ce752074d90bdfb151a
Guga Neri <guganeriss@gmail.com>
2020-09-21 07:58:42 -0700

    Python IO - Modulo 02

--- AluraPython/Python-IO/testa_escrita.py
+++ AluraPython/Python-IO/testa_escrita.py
@@ -1,5 +1,13 @@
 arquivo_contatos = open('contatos-escrita.csv', encoding='latin_1', mode='a')
 
-contato = '11,Carol,carol@carol.com.br\n'
+contatos = ['11,Carol,carol@carol.com.br\n',
+           '12,Ana,ana@ana.com.br\n',
+           '13,AnaOliveira,ana@ana.com.br\n',
+           '14,Anaclaudia,ana@ana.com.br\n',
+           '15,Felipe,felipa@felipe.com.br\n']
 
-arquivo_contatos.write(contato)
+for contato in contatos:
+    arquivo_contatos.write(contato)
+arquivo_contatos.flush()
+
+input('Pressione <Enter> para encerrrar o programa')
