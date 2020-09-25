e9d31cdf91fab618e878fbe9517c0801fb20ff20
Guga Neri <guganeriss@gmail.com>
2020-09-24 18:10:27 -0700

    Python IO - Trabalhando com exceptions

--- AluraPython/Python-IO/principal.py
+++ AluraPython/Python-IO/principal.py
@@ -1,6 +1,12 @@
-arquivo_contatos = open('contatos.csv', encoding='latin_1')
+try:
+    arquivo_contatos = open('contatos.csv', encoding='latin_1')
 
-#conteudo = arquivo_contatos.readline()
-
-for linha in arquivo_contatos:
-    print(linha, end='')
+    #conteudo = arquivo_contatos.readline()
+    for linha in arquivo_contatos:
+        print(linha, end='')
+except FileNotFoundError:
+    print('Arquivo não encontrado')
+except PermissionError:
+    print('Sem permissão de escrita')
+finally:
+    arquivo_contatos.close()
