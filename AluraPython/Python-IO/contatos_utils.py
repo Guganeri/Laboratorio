4d8106924c9b2e2ed0f0fa2643de70c4119a89ad
Guga Neri <guganeriss@gmail.com>
2021-03-11 19:05:54 -0800

    05.convertento contatos - convertendo para json

--- AluraPython/Python-IO/contatos_utils.py
+++ AluraPython/Python-IO/contatos_utils.py
@@ -1,4 +1,4 @@
-import csv
+import csv, pickle, json
 from contato import Contato
 
 
@@ -15,5 +15,28 @@ def csv_para_contatos(caminho, encoding='latin_1'):
 
             contato = Contato(id, nome, email)
             contatos.append(contato)
+
+    return contatos
+
+def contatos_para_pickle(contatos, caminho):
+    with open(caminho, mode='wb') as arquivo:        
+        pickle.dump(contatos, arquivo)
+
+def contatos_para_json(contatos, caminho):
+    with  open(caminho, mode='w') as arquivo:
+        json.dump(contatos, arquivo, default=_contato_para_json)
+
+def _contato_para_json(contato):
+    return contato.__dict__
+
+def json_para_contatos(caminho):
+    contatos = []
+
+    with open(caminho) as arquivo:
+        contatos_json = json.load(arquivo)
+
+        for contato in contatos_json:
+            c = Contato(**contato)
+            contatos.append(c)
 
     return contatos
--- AluraPython/Python-IO/principal.py
+++ AluraPython/Python-IO/principal.py
@@ -1,7 +1,11 @@
 import contatos_utils
 
 try:
-    contatos = contatos_utils.csv_para_contatos('contatos-escrita.csv')
+    #contatos = contatos_utils.csv_para_contatos('contatos-escrita.csv')
+    #contatos_utils.contatos_para_pickle(contatos, 'contatos.pickle')
+    #contatos_utils.contatos_para_json(contatos, 'contatos.json')
+
+    contatos = contatos_utils.json_para_contatos('contatos.json')
 
     for contato in contatos: 
         print(f'{contato.id} - {contato.nome} - {contato.email}')
@@ -9,4 +13,4 @@ try:
 except FileNotFoundError:
     print('Arquivo não encontrado')
 except PermissionError:
-    print('Sem permissão de escrita')    
+    print('Sem permissão de escrita')
