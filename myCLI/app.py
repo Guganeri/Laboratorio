35ba98a2332965973dccd23d95b79319c8354b47
Gustavo Neri <guganeriss@gmail.com>
2024-04-29 13:42:57 +0100

    fix menu

--- myCLI/app.py
+++ myCLI/app.py
@@ -2,13 +2,6 @@ import os
 
 def showtime():
     print("""
-░█████╗░██████╗░██████╗░░█████╗░███████╗███╗░░██╗░█████╗░
-██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝████╗░██║██╔══██╗
-███████║██████╔╝██████╔╝██║░░██║█████╗░░██╔██╗██║███████║
-██╔══██║██╔═══╝░██╔═══╝░██║░░██║██╔══╝░░██║╚████║██╔══██║
-██║░░██║██║░░░░░██║░░░░░╚█████╔╝███████╗██║░╚███║██║░░██║
-╚═╝░░╚═╝╚═╝░░░░░╚═╝░░░░░░╚════╝░╚══════╝╚═╝░░╚══╝╚═╝░░╚═╝
-
 ██╗░░██╗░█████╗░██████╗░██████╗░███████╗███╗░░██╗██╗███╗░░██╗░██████╗░
 ██║░░██║██╔══██╗██╔══██╗██╔══██╗██╔════╝████╗░██║██║████╗░██║██╔════╝░
 ███████║███████║██████╔╝██║░░██║█████╗░░██╔██╗██║██║██╔██╗██║██║░░██╗░
@@ -23,23 +16,32 @@ def showmenu():
     print('2. APM Verify')
     print('3. Exit\n')
 
+def invalid_option():
+    print('Invalid option!\n')
+    input('Press Enter to continue...')
+    main()
+
 def finish_app():
     os.system('clear')
     print('Poweroff...\n')
 
 def choose_option():
-    op = int(input(''))
-    match op:
-        case 1:
-            print('Agent Verify')
-        case 2:
-            print('APM Verify')
-        case 3:
-            finish_app()
-        case _:
-            print('Invalid option!')
+    try:
+        op = int(input(''))
+        match op:
+            case 1:
+                print('Agent Verify')
+            case 2:
+                print('APM Verify')
+            case 3:
+                finish_app()
+            case _:
+                invalid_option()
+    except:
+        invalid_option()
 
 def main():
+    os.system('clear')
     showtime()
     showmenu()
     choose_option()
